415a6562bf91cd7cbbae48cbae618ea38faf0172
swalecko <swalecko@gmail.com>
2016-04-01 13:31:24 +0200

    removed unused lines

--- polo-caller.py
+++ polo-caller.py
@@ -2,17 +2,7 @@ import polowrapper
 import requests
 import json
 
-#response = requests.Request('https://poloniex.com/public?command=returnTicker')
-#response = requests.post('https://poloniex.com/public?command=returnTicker')
-#output = json.loads(response.text)['BTC_XMR']['percentChange']
-#output = json.loads(response.read())
-#print (output)
-
-
 a = polowrapper.poloniex('','')
-#a.returnTicker()
-#test = a.returnMarketTradeHistory('BTC_XMR')
-#print (json.dumps(test, indent=4, sort_keys=True))
 
 retTicker = a.returnTicker()
 print ("BTC/XMR Prize: " + retTicker['BTC_XMR']['last'] + " BTC")
@@ -22,7 +12,3 @@ print ("Current Balance: " + retBalance['XMR'] + " XMR")
 print ("Open Orders:")
 openOrders = a.returnOpenOrders('BTC_XMR')
 print (openOrders)
-#print ("Trade History:")
-#tradeHistory = a.returnTradeHistory('BTC_XMR')
-#print (json.dumps(tradeHistory, indent=4, sort_keys=True))
-
